13ed4e326734361f2ff16e1755ff34e0283bed7c
rias-z <chestermx88@gmail.com>
2017-12-21 16:42:53 +0900

    最適化

---
+++ create-character-app/src/Component/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>create-character-app</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
