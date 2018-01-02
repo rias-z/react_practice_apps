350ae14a57813929cfa94b0c31fcd43c0e952476
rias-z <chestermx88@gmail.com>
2018-01-03 00:43:13 +0900

    react-router-dom入れた

--- multi-chat-app/src/Component/App.js
+++ multi-chat-app/src/Component/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom'
+
+const Login = () => (
+  <div>
+    <h1>Login</h1>
+    <a href="/admin">go Admin</a>
+  </div>
+)
+
+const Admin = () => (
+  <div>
+    <h1>Admin</h1>
+    <a href="/">go Login</a>
+  </div>
+)
+
+const Main = () => (
+  <Switch>
+    <Route exact path='/' component={Login} />
+    <Route path='/Admin' component={Admin} />
+  </Switch>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>App</h1>
+        <Main />
       </div>
     );
   }
