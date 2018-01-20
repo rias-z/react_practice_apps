629517d2f7ca01d16d827d12c2560e79b2a09148
rias-z <chestermx88@gmail.com>
2018-01-21 05:26:53 +0900

    画像のアップロードとプレビュー，画像の保存をできるようにした

--- image-post-app/src/Component/App.js
+++ image-post-app/src/Component/App.js
@@ -1,11 +1,84 @@
 import React, { Component } from 'react'
+import request from 'superagent'
 
+
+const createObjectURL = (window.URL || window.webkitURL).createObjectURL ||
+  window.createObjectURL
 
 class ImageUploadForm extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      imgSrc: '',
+      message: ''
+    }
+  }
+
+  handleChange(e) {
+    e.preventDefault()
+    const imageUrl = createObjectURL(e.target.files[0])
+
+    this.setState({
+      imgSrc: imageUrl
+    })
+  }
+
+  handlePost(e) {
+    e.preventDefault()
+
+    const data = new FormData()
+    data.append('file', e.target.image.files[0])
+
+    request
+      .post('http://localhost:5000/post/image')
+      .attach('image', e.target.image.files[0])
+      .then(res => {
+        console.log("ok")
+
+        this.setState({
+          message: '登録できました'
+        })
+      })
+      .catch(err => {
+        console.log("err")
+
+        this.setState({
+          message: '失敗しました'
+        })
+      })
+  }
+
   render() {
     return (
       <div>
         <h2>ImageUploadForm</h2>
+
+        <form onSubmit={this.handlePost.bind(this)}>
+          <input type='file' name='image' onChange={this.handleChange.bind(this)}/>
+
+          {(() => {
+            if (this.state.imgSrc){
+              return (
+                <div>
+                  <br />プレビュー (100*100)<br />
+                  <img alt='img' src={this.state.imgSrc} width='100' height='100'/>
+                </div>
+              )
+            }
+          })()}
+
+          <br />
+          <input type='submit' value='post' />
+          {(() => {
+            if (this.state.message){
+              return (
+                <div>{this.state.message}</div>
+              )
+            }
+          })()}
+        </form>
+
       </div>
     )
   }
